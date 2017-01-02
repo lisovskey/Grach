96812139df9dfa652337529298ccd56fa9ef2cd6
lisovskey <ritofzeed@gmail.com>
2017-01-02 19:59:15 +0300

    go

---
+++ constants.py
@@ -0,0 +1,14 @@
+'''
+капсом пишем жопай
+'''
+
+TOKEN = '280995361:AAFPietXTGxRttwiE8GIn4xVRGBW6VJmXVk'
+
+WHAT = 'что'
+IGNORANCE = 'не понял щас'
+HELP = 'могу василюка хуй показать'
+START = 'я гриша'
+GREET = 'привет'
+PARTING = 'пока'
+
+DICK = 'AgADAgADsqcxG619BApGPqqhwbre04fUgQ0ABEEa0kzVKn3I0OADAAEC'
---
+++ main.py
@@ -0,0 +1,121 @@
+'''
+умеет мало
+работаем дальше
+'''
+
+import API
+import constants
+
+bot = API.TeleBot(constants.TOKEN)
+print(bot.get_me())
+
+def log(message, answer):
+    '''
+    логируем в консоль
+    '''
+    print('----------------------------------------------------------------')
+    print('{0} {1} (id {2})'.format(message.from_user.first_name,
+                                    message.from_user.last_name,
+                                    str(message.from_user.id)), end='')
+    if message.chat.id != message.from_user.id:
+        print(' in {0} (id {1}): '.format(message.chat.title,
+                                          message.chat.id))
+    else:
+        print(': ')
+    print(' - {}'.format(message.text))
+    print('Отвечаю: ')
+    print(' - {}'.format(answer))
+
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    '''
+    чо делать, если халп
+    '''
+    answer = constants.HELP
+    log(message, answer)
+    bot.send_message(message.chat.id, answer)
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def handle_text(message):
+    '''
+    как отвечать
+    '''
+    answer = constants.IGNORANCE
+    text_message = message.text.lower()
+
+    leave = False
+    reaction = False
+
+    if message.chat.id != message.from_user.id:
+        if ('грач' in text_message or
+            'гриша' in text_message or
+            'григорий' in text_message):
+            reaction = True
+    else:
+        reaction = True
+
+    if reaction:
+
+        if ('прив' in text_message or
+            'привет' in text_message or
+            'здравствуй' in text_message or
+            'хай' in text_message or
+            'хаюх' in text_message or
+            'хаю хай' in text_message or
+            'дратут' in text_message or
+            'здаров' in text_message):
+            answer = constants.GREET
+
+        elif ('все давай' in text_message or
+              'всё давай' in text_message or
+              'датвиданья' in text_message or
+              'до связи' in text_message or
+              'до свидани' in text_message or
+              'досвидан' in text_message or (
+                  'пока' in text_message and
+                  'покаж' not in text_message) or (
+                      'пака' in text_message and
+                      'пакаж' not in text_message)):
+            answer = constants.PARTING
+
+        if ('умееш' in text_message or
+            'можеш' in text_message) and (
+                '?' in text_message or
+                'че' in text_message or
+                'чё' in text_message or
+                'чо' in text_message or
+                'что' in text_message):
+            answer = constants.HELP
+
+        elif ('хуй' in text_message or
+              'член' in text_message) and (
+                  'василюк' in text_message or
+                  'денис' in text_message or
+                  'покажи' in text_message or
+                  'скинь' in text_message):
+            answer = constants.DICK
+
+        elif ('отсюда' in text_message or
+              'отсюдо' in text_message) and (
+                  'нахуй' in text_message or
+                  'на хуй' in text_message or
+                  'нахер' in text_message or
+                  'на хер' in text_message):
+            answer = constants.PARTING
+
+            if message.chat.id != message.from_user.id:
+                leave = True
+
+        if (text_message == 'грач' or
+            text_message == 'гриша' or
+            text_message == 'григорий'):
+            answer = constants.WHAT
+
+        log(message, answer)
+        bot.send_message(message.chat.id, answer)
+
+        if leave:
+            bot.leave_chat(message.chat.id)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True, interval=0)
