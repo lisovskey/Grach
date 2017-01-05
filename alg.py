548b24204836a745f444553af61470a99509c83d
hetchback <still2405@gmail.com>
2017-01-05 21:22:48 +0300

    Update alg.py

--- alg.py
+++ alg.py
@@ -6,38 +6,32 @@ with open('content.json') as json_data:
     
 text_message = str(input())
 
-
-flag = False
+parts = 0
 word = None
+no_commands = True
+need_name = 0
 
-'''command = DATABASE['commands'][0]
-print(command)
-text_list = command['text']
-print(text_list)
-text = text_list[0]
-print(text)
-word = text['part']
-print(word)
-word = word[0]
-print(word)
-''' 
+if any(name in text_message for name in DATABASE['config']['bot_names']):
+    need_name = 1
 for command in DATABASE['commands']:
     for text in command['text']:
         if type(text) is dict:
             for word in text['part']:
                 if word in text_message:
-                    flag = True
-                    break
-                else:
-                    flag = False
+                    i = text_message.find(word)
+                    if (i + len(word)) < len(text_message):
+                        if text_message[i + len(word)] != "\a" or text_message[i + len(word) + 2] != "\s":
+                            parts += 1
+                            break
+                    elif (i + len(word)) == len(text_message):
+                        parts += 1
+                        break
         else:
             print(text)
-    # if any(name in text_message for name in DATABASE['config']['bot_names']):
-    if flag:
-        print(random.choice(command['output']))
-        flag = False
-        break
-#    else:
-#        print(random.choice(DATABASE['config']['bot_call_output']))
-        
-        
+    if need_name > 0:
+        if parts == command['parts']:
+            no_commands = False
+            print(random.choice(command['answer']))
+    parts = 0
+if no_commands and need_name > 0:
+    print(random.choice(DATABASE['config']['bot_call_output']))
