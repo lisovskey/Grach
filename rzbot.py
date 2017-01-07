c12aad8a6ec2e3c6fbf8ea09fed710b9a15d99be
lisovskey <ritofzeed@gmail.com>
2017-01-07 19:13:51 +0300

    fix

--- rzbot.py
+++ rzbot.py
@@ -137,7 +137,7 @@ class RZTeleBot(telebot.TeleBot):
                 answer = str(math.factorial(numbers[0]))
                 if numbers[0] > 100:
                     answer += ' но это не точно'
-                return answer.format(grouping=True)
+                return '{: }'.format(answer)
             else:
                 return 'неенененене'
 
