7bd04504c154010514e047adb32839c94203234a
lisovskey <ritofzeed@gmail.com>
2017-01-10 19:06:22 +0300

    fix

--- unloader.py
+++ unloader.py
@@ -58,7 +58,7 @@ class Unloader:
         schedule += pretext + ' ' + tmp_week + ':'
 
         resp = requests.get('https://www.bsuir.by/schedule/rest/schedule/' + str(group))
-        soup = BeautifulStoneSoup(features="xml", resp.content)
+        soup = BeautifulStoneSoup(resp.content, features="xml")
         day = soup.find_all('weekDay', text=week_day)
         if not day:
             return 'отдыхает'
