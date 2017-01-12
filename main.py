ddcb20733084bf87eee0a5656ea65833262338f6
lisovskey <ritofzeed@gmail.com>
2017-01-13 01:40:02 +0300

    fix conditions

--- main.py
+++ main.py
@@ -50,9 +50,9 @@ def handle_schedule(message, text_message):
 
     delta = 1
     if '/' not in message.text:
-        if any(DATABASE['dictionary']['today'] in text_message):
+        if any(DATABASE['dictionary']['today']) in text_message:
             delta = 0
-        elif any(DATABASE['dictionary']['after_tomorrow'] in text_message):
+        elif any(DATABASE['dictionary']['after_tomorrow']) in text_message:
             delta = 2
 
     for user in DATABASE['users']:
