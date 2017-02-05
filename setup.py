211fa6859821c1089d84f1514538a7e7fb2db421
lisovskey <ritofzeed@gmail.com>
2017-02-05 15:14:46 +0300

    requirements for code analysis

--- setup.py
+++ setup.py
@@ -7,13 +7,26 @@
 from setuptools import setup
 import src
 
+def requirements():
+    '''
+    устанавливаю все
+    '''
+    requirements_list = []
+
+    with open('requirements.txt') as reqs:
+        for install in reqs:
+            requirements_list.append(install.strip())
+
+    return requirements_list
+
 setup(
     name='grach',
     version=src.__version__,
     description='Telegram Bot',
     author='lisovskey',
+    license='MIT',
     url='https://github.com/lisovskey/grach',
     packages=['src'],
-    install_requires=['requests', 'bs4', 'pyTelegramBotAPI'],
+    install_requires=requirements(),
     data_files=['config', ['src\\config.py']]
 )
